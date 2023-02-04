d0ac89c696c23e0e6e0f353635f8b08a0c68277b
Danilo Nascimento <daconnas.dcn@gmail.com>
2023-02-04 19:22:28 -0300

    fix: permitindo todos os hosts

--- igreja_ibm/settings.py
+++ igreja_ibm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pw6lnh^ea3d(zr9xzpy-s^(y98jrpu=-dgx!o!imloz!-p99_^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
