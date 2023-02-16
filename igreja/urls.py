5d1cee075719848ad5aec8716e8af54871b7084f
Danilo Nascimento <daconnas.dcn@gmail.com>
2023-02-16 12:44:12 -0300

    voltando a servir estáticos

--- igreja/urls.py
+++ igreja/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("igreja.apps.web.urls")),
 ] 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_title = "IBM"
 admin.site.site_header = "Administração - IBM"
